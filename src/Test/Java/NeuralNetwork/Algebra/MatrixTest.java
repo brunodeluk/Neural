c54308494c5ae108ca4b21a969e9deca8fe9f5dd
brunodeluca <brunodeluca_96@hotmail.com>
2017-02-26 20:25:11 -0300

    changed sum() method from matrixText class

--- src/Test/Java/NeuralNetwork/Algebra/MatrixTest.java
+++ src/Test/Java/NeuralNetwork/Algebra/MatrixTest.java
@@ -60,13 +60,6 @@ public class MatrixTest {
 
     @Test
     public void sum() throws Exception {
-        int[][] matrix = {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 }
-        };
-
-        System.out.println(matrix[0][1]);
     }
 
     @Test
